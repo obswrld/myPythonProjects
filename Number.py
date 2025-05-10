d62236b5fe92a84b36f3b8cb6291c2341643dea1
Republic Oba <republicoba1@gmail.com>
2025-05-10 20:49:13 +0200

    delete

--- Number.py
+++
@@ -1,7 +0,0 @@
-number1 = int(input('Enter number:  '))
-number2 = int(input('Enter number:  '))
-number3 = int(input('Enter number:  '))
-number4 = int(input('Enter number:  '))
-number5 = int(input('Enter number:  '))
-
-print(number1,     number2,     number3,     number4,     number5)
--- NumberValidation.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Enter number here:  "))
-counter = int(0)
-while number != 1 and number != 2:
-	number = int(input("Enter number again:   "))
-	counter = counter + 1
-print(f"You failed the guess {counter} times")
-	
